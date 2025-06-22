ccd2e628e1d4afa910db439c649f635780cec860
Malizma333 <malizma333@gmail.com>
2025-06-22 11:50:47 -0500

    edited tests to parse fixtures

--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,34 @@ pub use game::*;
 
 #[cfg(test)]
 mod tests {
+    use std::fs;
+    use lr_formatter_rs::trackjson::read;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn crash() {
+      let track_bytes = fs::read_to_string("./fixtures/crash.track.json").expect("Failed to read file");
+      let track = read(&track_bytes).expect("Failed to parse file");
+      println!("{}", track.title);
+    }
+
+    #[test]
+    fn cycloid() {
+      let track_bytes = fs::read_to_string("./fixtures/cycloid.track.json").expect("Failed to read file");
+      let track = read(&track_bytes).expect("Failed to parse file");
+      println!("{}", track.title);
+    }
+
+    #[test]
+    fn legacy_test() {
+      let track_bytes = fs::read_to_string("./fixtures/legacyTestTrack.track.json").expect("Failed to read file");
+      let track = read(&track_bytes).expect("Failed to parse file");
+      println!("{}", track.title);
+    }
+
+    #[test]
+    fn modern_test() {
+      let track_bytes = fs::read_to_string("./fixtures/testTrack.track.json").expect("Failed to read file");
+      let track = read(&track_bytes).expect("Failed to parse file");
+      println!("{}", track.title);
     }
 }
